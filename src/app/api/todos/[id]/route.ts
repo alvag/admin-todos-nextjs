27f50a49d6956b2c7d0d468f35f895f7b542ff2d
Max Alva Garcia <alva85@gmail.com>
2023-06-26 07:18:22 -0500

    Actualizar entradas - PUT

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -19,3 +19,20 @@ export async function GET( req: Request, { params }: Segments ) {
 
     return NextResponse.json( todo );
 }
+
+export async function PUT( req: Request, { params }: Segments ) {
+    const { id } = params;
+
+    let todo = await prisma.todo.findFirst( { where: { id } } );
+
+    if ( !todo ) {
+        return NextResponse.json( { error: 'Todo not found' }, { status: 404 } );
+    }
+
+    const data = await req.json();
+
+    todo = await prisma.todo.update( { where: { id }, data } );
+
+    return NextResponse.json( todo );
+
+}
