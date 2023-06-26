904175deb1b148d26fe005780399b016e816647d
Max Alva Garcia <alva85@gmail.com>
2023-06-26 06:24:02 -0500

    Paginacion Simple

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -2,7 +2,23 @@ import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 
 export async function GET( req: Request ) {
-    const todos = await prisma.todo.findMany();
+
+    const { searchParams } = new URL( req.url );
+    const take = Number( searchParams.get( 'take' ) || '10' );
+    const skip = Number( searchParams.get( 'skip' ) || '0' );
+
+    if ( isNaN( take ) ) {
+        return NextResponse.json( { error: 'Invalid take parameter' }, { status: 400 } );
+    }
+
+    if ( isNaN( skip ) ) {
+        return NextResponse.json( { error: 'Invalid skip parameter' }, { status: 400 } );
+    }
+
+    const todos = await prisma.todo.findMany( {
+        take,
+        skip,
+    } );
 
     return NextResponse.json( todos );
 }
