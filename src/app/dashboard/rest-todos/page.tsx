6300aab218dfbf1843a4870267e3dcd92962fa9f
Max Alva Garcia <alva85@gmail.com>
2023-06-28 08:37:46 -0500

    Diseño pagina de TODOs

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,11 +1,23 @@
 import { FC } from 'react';
+import prisma from '@/lib/prisma';
+import { TodosGrid } from '@/components';
+
+
+export const metadata = {
+    title: 'Listado de Todos',
+    description: 'Listado de todos consumiendo una API REST',
+}
 
 interface RestTodosProps {
 }
 
-const RestTodosPage: FC<RestTodosProps> = ( {} ) => {
+const RestTodosPage: FC<RestTodosProps> = async ( {} ) => {
+    const todos = await prisma.todo.findMany( { orderBy: { description: 'asc' } } );
+
     return (
-        <div>RestTodos Page</div>
+        <div>
+            <TodosGrid todos={ todos }/>
+        </div>
     );
 };
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { Sidebar } from '@/components/sidebar/Sidebar';
 export { SidebarItem } from '@/components/sidebar/SidebarItem';
 export { TopMenu } from './TopMenu'
 export { WidgetItem } from './dashboard/WidgetItem'
+export { TodosGrid } from './todos/TodosGrid';
