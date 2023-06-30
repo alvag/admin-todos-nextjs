80c262627442b0d83dec0f817dfe4423d46166bd
Max Alva Garcia <alva85@gmail.com>
2023-06-30 16:16:12 -0500

    Mostrar informacion del usuario

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -9,6 +9,8 @@ import {
     IoCodeWorkingOutline,
     IoListOutline
 } from 'react-icons/io5';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 const menuItems = [
     {
@@ -38,7 +40,10 @@ const menuItems = [
     },
 ]
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+    const session = await getServerSession( authOptions );
+    const user = session ? session?.user : {};
+
     return (
         <aside
             className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
@@ -52,10 +57,11 @@ export const Sidebar = () => {
                 </div>
 
                 <div className="mt-8 text-center">
-                    <Image src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp" alt=""
+                    <Image src={ user?.image || 'https://tailus.io/sources/blocks/stats-cards/preview/images/logo.svg' }
+                           alt=""
                            width={ 40 } height={ 40 }
                            className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"/>
-                    <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">Cynthia J. Watts</h5>
+                    <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">{ user?.name || 'Cynthia J. Watts' }</h5>
                     <span className="hidden text-gray-400 lg:block">Admin</span>
                 </div>
 
