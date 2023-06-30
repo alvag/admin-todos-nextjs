45d831de2975a39a0dda2fa464016d70bf7d7f98
Max Alva Garcia <alva85@gmail.com>
2023-06-30 16:07:48 -0500

    Informacion del usuario server side

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,28 @@
 import { FC } from 'react';
 import { WidgetItem } from '@/components';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
 
 interface DashboardProps {
 }
 
-const DashboardPage: FC<DashboardProps> = ( {} ) => {
+const DashboardPage: FC<DashboardProps> = async ( {} ) => {
+    const session = await getServerSession( authOptions );
+
+    if ( !session ) {
+        redirect( '/api/auth/signin' );
+    }
+
     return (
-        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-            {/*<WidgetItem/>*/ }
+        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2">
+            <WidgetItem title="Usuario conectado Server Side">
+                <div className="flex flex-col">
+                    <span>{ session.user?.name }</span>
+                    <span>{ session.user?.email }</span>
+                    <span>{ session.user?.image }</span>
+                </div>
+            </WidgetItem>
         </div>
     );
 };
